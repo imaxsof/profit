c36897fdd6de12966ea6647e5c5037650aa94edc
Maxim Sofronov <imaxsof@gmail.com>
2018-11-19 14:42:31 +0300

    no message

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-document.querySelector('button').addEventListener('click', function() {
+document.querySelector('#submit').addEventListener('click', function() {
 	var www = document.querySelector('#price').value * 1000 / document.querySelector('#ves').value;
 
 	document.querySelector('#price').value = document.querySelector('#ves').value = '';
 
 	document.querySelector('.result').append(Math.round(www) + ' • ');
+});
+
+document.querySelector('#clear').addEventListener('click', function() {
+	document.querySelector('.result').innerHTML = '';
 });
